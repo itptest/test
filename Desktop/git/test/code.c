63f6851e3ebf182a4a5dba81e3ad2b463e661128
t.prokopenko <elwinter@winter.local>
2018-01-22 12:09:14 +0300

    2nd commit

---
+++ Desktop/git/test/code.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+      double firstNumber, secondNumber, temporaryVariable;
+
+      printf("Enter first number: ");
+      scanf("%lf", &firstNumber);
+
+      printf("Enter second number: ");
+      scanf("%lf",&secondNumber);
+
+      // Value of firstNumber is assigned to temporaryVariable
+      temporaryVariable = firstNumber;
+
+      // Value of secondNumber is assigned to firstNumber
+      firstNumber = secondNumber;
+
+      // Value of temporaryVariable (which contains the initial value of firstNumber) is assigned to secondNumber
+      secondNumber = temporaryVariable;
+
+      printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+      printf("After swapping, secondNumber = %.2lf", secondNumber);
+
+      return 0;
+	  
+	      int year;
+
+    printf("Enter a year: ");
+    scanf("%d",&year);
+
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+            // year is divisible by 400, hence the year is a leap year
+            if ( year%400 == 0)
+                printf("%d is a leap year.", year);
+            else
+                printf("%d is not a leap year.", year);
+        }
+        else
+            printf("%d is a leap year.", year );
+    }
+    else
+        printf("%d is not a leap year.", year);
+    
+    return 0;
+}
